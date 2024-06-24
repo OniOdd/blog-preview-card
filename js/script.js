ab3c17869a5045f21216b52ce5bb5b24776933a2
Mykyta Kavetskyi <103828218+OniOdd@users.noreply.github.com>
2024-06-24 14:10:26 +0300

    Added event listeners focus and blur.

--- js/script.js
+++ js/script.js
@@ -5,6 +5,8 @@ const link = document.getElementById("link");
 
 link.addEventListener("mouseenter", () => toggleClassActive());
 link.addEventListener("mouseleave", () => toggleClassActive());
+link.addEventListener("focus", () => toggleClassActive());
+link.addEventListener("blur", () => toggleClassActive());
 
 function toggleClassActive() {
   card.classList.toggle("active");
